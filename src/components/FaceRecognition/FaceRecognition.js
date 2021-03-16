9487fa47a292c4c41928a1c90b29f7dd16ffa49a
ANK-dev <arielnogueirak@gmail.com>
2021-03-16 14:47:29 -0300

    Fixed invalid key error

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,10 +5,19 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
         {
           boxes.map(box => (
-            <div key={box.topRow} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            <div
+              key={box.topRow + box.leftCol}
+              className='bounding-box'
+              style={{
+                top: box.topRow,
+                right: box.rightCol,
+                bottom: box.bottomRow,
+                left: box.leftCol
+              }}
+            ></div>
           ))
         }
       </div>
